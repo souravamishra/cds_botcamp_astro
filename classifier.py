c5cab9ccbec68364aae8b9e0ec382bd9124ec4cc
Sourava Mishra <sourav7mishra@gmail.com>
2015-04-10 10:11:02 +0200

    model02

--- classifier.py
+++ classifier.py
@@ -1,20 +1,21 @@
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+#from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
-
+ 
 class Classifier(BaseEstimator):
     def __init__(self):
         self.clf = Pipeline([
             ('imputer', Imputer(strategy='most_frequent')),
-            ('rf', RandomForestClassifier(max_depth=2, n_estimators=10))
+            ('sgd', SGDClassifier( loss = 'modified_huber', penalty = 'elasticnet', alpha = 1.4728334788264625, l1_ratio = 0.34555431482496163 ))
         ])
-
+ 
     def fit(self, X, y):
         self.clf.fit(X, y)
-
+ 
     def predict(self, X):
         return self.clf.predict(X)
-
+ 
     def predict_proba(self, X):
         return self.clf.predict_proba(X)
